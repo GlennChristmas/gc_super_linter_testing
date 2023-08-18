552c79cace921698267b5338d3944cb4e367d6a4
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2023-08-18 17:02:25 +0100

    add linter trigger for PR

---
+++ linter_trigger_2.R
@@ -0,0 +1,11 @@
+#here is some poorly formatted code to try and trigger the linter. This first line is a very long comment that should probably trigger a recommendation to be split up or removed altogether.
+
+library(tibble)
+
+trib1 <- tibble::tribble(~"item1",~"item2",~"item3",1,2,3,1,2,3,1,2,3)
+
+#white
+
+
+
+#space
