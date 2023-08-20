fb422f5822266d67449a899e251c8f9b6508ae99
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2023-08-20 14:58:49 +0100

    this should trigger the linter

--- this_should_trigger_linter.R
+++
@@ -1,3 +0,0 @@
-#this should trigger the linter this should trigger the linter this should trigger the linter this should trigger the linter this should trigger the linter this should trigger the linter 
-
-trib1 <- tibble::tribble("item1","item2",1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2)
---
+++ this_should_trigger_linter_4.R
@@ -0,0 +1,3 @@
+#this should trigger the linter
+trib1 <- tibble::tribble("item1", "item2", 1, 2,
+                         1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2)
